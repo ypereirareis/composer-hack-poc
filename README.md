3abd2d131b9b8a746fcab69399ed4950b5853e48
Yannick Pereira-Reis <yannick.pereira.reis@gmail.com>
2021-02-25 16:41:15 +0100

    First commit for composer hack poc

---
+++ README.md
@@ -0,0 +1,7 @@
+# email-template-bundle
+
+Project to show that dependency source must be checked (private ou public).
+
+Public packagist repo :  https://packagist.org/packages/sfk/
+
+
---
+++ src/Check.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sfk\ComposerHackPoc;
+
+/**
+ * Class Check
+ */
+class Check
+{
+    public static function getMessage()
+    {
+        return 'Hello from github public repo';
+    }
+}
